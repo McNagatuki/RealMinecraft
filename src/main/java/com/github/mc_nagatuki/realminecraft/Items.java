f3d1a7e79ca2592091ea7665296e4c841b96d3ea
McNagatuki <kikihiji.fuumi5447491@gmail.com>
2020-11-26 01:33:47 +0900

    review: add "final" to member variables

--- src/main/java/com/github/mc_nagatuki/realminecraft/Items.java
+++ src/main/java/com/github/mc_nagatuki/realminecraft/Items.java
@@ -4,23 +4,23 @@ import org.bukkit.Material;
 
 public class Items {
     public static class Detector{
-        public static Material material = Material.BLAZE_ROD;
-        public static String id = "minecraft:blaze_rod";
-        public static String name = "地雷探知機";
-        public static String color = "green";
+        public static final Material material = Material.BLAZE_ROD;
+        public static final String id = "minecraft:blaze_rod";
+        public static final String name = "地雷探知機";
+        public static final String color = "green";
     }
 
     public static class Installer{
-        public static Material material = Material.BOWL;
-        public static String id = "minecraft:bowl";
-        public static String name = "ほんものの地雷";
-        public static String color = "red";
+        public static final Material material = Material.BOWL;
+        public static final String id = "minecraft:bowl";
+        public static final String name = "ほんものの地雷";
+        public static final String color = "red";
     }
 
     public static class Sweeper{
-        public static Material material = Material.WHEAT;
-        public static String id = "minecraft:wheat";
-        public static String name = "マインスイーパー";
-        public static String color = "yellow";
+        public static final Material material = Material.WHEAT;
+        public static final String id = "minecraft:wheat";
+        public static final String name = "マインスイーパー";
+        public static final String color = "yellow";
     }
 }
