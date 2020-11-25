85f065cf33f51f44b15a5d5be9450adc21e624ae
McNagatuki <kikihiji.fuumi5447491@gmail.com>
2020-11-26 02:17:21 +0900

    review: adjust PlayerMovementListener to RealMinecraft singleton

--- src/main/java/com/github/mc_nagatuki/realminecraft/PlayerMovementListener.java
+++ src/main/java/com/github/mc_nagatuki/realminecraft/PlayerMovementListener.java
@@ -1,5 +1,6 @@
 package com.github.mc_nagatuki.realminecraft;
 
+import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -8,27 +9,21 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 public class PlayerMovementListener implements Listener {
-    private RealMinecraft plugin;
-
-    public PlayerMovementListener(RealMinecraft plugin){
-        this.plugin = plugin;
-    }
-
     @EventHandler(ignoreCancelled = true)
-    public void onPlayerMoveEvent(PlayerMoveEvent e){
+    public void onPlayerMoveEvent(PlayerMoveEvent e) {
         // プラグインがオフなら何もしない
-        if(!this.plugin.getActivated()) return;
+        if (!RealMinecraft.plugin.getRealConfig().isActivated()) return;
 
         // プレイヤーを取得
         Player p = e.getPlayer();
 
         // プレイヤーがクリエイティブかスペクテイターならなにもしない
-        if(p.getGameMode() == GameMode.CREATIVE || p.getGameMode() == GameMode.SPECTATOR) return;
+        if (p.getGameMode() == GameMode.CREATIVE || p.getGameMode() == GameMode.SPECTATOR) return;
 
         // プレイヤーが浮いていたら何もしない
         // ただし乗り物に乗っている場合を除く
         // 厳密にやるならy座標を見た方がいいらしい
-        if(!(p.isOnGround() || p.isInsideVehicle())) return;
+        if (!(p.isOnGround() || p.isInsideVehicle())) return;
 
         //移動先の座標を得る
         Location loc = e.getTo();
@@ -36,14 +31,14 @@ public class PlayerMovementListener implements Listener {
         int z = loc.getBlockZ();
 
         // 当該座標に地雷がなければ何もしない
-        if(!this.plugin.getMineManager().hasMine(x, z)) return;
+        if (!RealMinecraft.plugin.getMineManager().hasMine(x, z)) return;
 
         // 爆発処理
-        this.plugin.getPlayerExploder().explode(loc, p, this.plugin.getPower(), this.plugin.getDamage());
+        RealMinecraft.plugin.getPlayerExploder().explode(loc, p, RealMinecraft.plugin.getRealConfig().getPower(), RealMinecraft.plugin.getRealConfig().getDamage());
 
         // 地雷除去（爆発しても地雷がなくならないようにするならここを変更）
-        this.plugin.getMineManager().demine(x, z);
+        RealMinecraft.plugin.getMineManager().demine(x, z);
 
-        this.plugin.getServer().broadcastMessage("§9"+e.getPlayer().getName()+"は地雷を踏み抜いた");
+        RealMinecraft.plugin.getServer().broadcastMessage(ChatColor.BLUE + e.getPlayer().getName() + "は地雷を踏み抜いた");
     }
 }
