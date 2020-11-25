69bfe98f07d1009ba4ad9a06f602f500c37a17d9
McNagatuki <kikihiji.fuumi5447491@gmail.com>
2020-11-26 01:50:27 +0900

    review: make RealMinecraft singleton こうすることで、この関数のインスタンスにアクセスするのに`RealMinecraft.plugin`で済むようになる。

--- src/main/java/com/github/mc_nagatuki/realminecraft/RealMinecraft.java
+++ src/main/java/com/github/mc_nagatuki/realminecraft/RealMinecraft.java
@@ -3,12 +3,16 @@ package com.github.mc_nagatuki.realminecraft;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RealMinecraft extends JavaPlugin {
+    public static RealMinecraft plugin;
+
     private MineManager mm;
     private PlayerExploder pe;
     private RealConfiguration config;
 
     @Override
     public void onEnable() {
+        this.plugin = this;
+
         this.mm = new MineManager();
         this.pe = new PlayerExploder();
         this.config = new RealConfiguration();
