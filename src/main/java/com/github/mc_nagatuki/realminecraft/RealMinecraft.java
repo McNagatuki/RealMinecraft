609daf259511d3efcc9f7bd52f00b9ff31f1bbb9
McNagatuki <kikihiji.fuumi5447491@gmail.com>
2020-11-26 01:24:07 +0900

    review: remove unnecessary member variables and move some member

--- src/main/java/com/github/mc_nagatuki/realminecraft/RealMinecraft.java
+++ src/main/java/com/github/mc_nagatuki/realminecraft/RealMinecraft.java
@@ -3,94 +3,33 @@ package com.github.mc_nagatuki.realminecraft;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class RealMinecraft extends JavaPlugin {
-    private PlayerMovementListener pml;
-    private PlayerClickListener pcl;
     private MineManager mm;
     private PlayerExploder pe;
-    private CommandManager cmdMng;
-
-    private int power = 1;
-    private int x1 = 0, z1 = 0, x2 = 0, z2 = 0;
-    private double damage = 5.0;
-    private double probability = 0.3;
-    private boolean activated = false;
+    private RealConfiguration config;
 
     @Override
     public void onEnable() {
         this.mm = new MineManager();
         this.pe = new PlayerExploder();
+        this.config = new RealConfiguration();
 
-        this.pml = new PlayerMovementListener(this);
-        this.getServer().getPluginManager().registerEvents(this.pml, this);
+        // events
+        this.getServer().getPluginManager().registerEvents(new PlayerMovementListener(this), this);
+        this.getServer().getPluginManager().registerEvents(new PlayerClickListener(this), this);
 
-        this.pcl = new PlayerClickListener(this);
-        this.getServer().getPluginManager().registerEvents(this.pcl, this);
-
-        this.cmdMng = new CommandManager(this);
-        this.getCommand("real").setExecutor(this.cmdMng);
+        // commands
+        this.getCommand("real").setExecutor(new CommandManager(this));
     }
 
-    public MineManager getMineManager(){
+    public MineManager getMineManager() {
         return this.mm;
     }
 
-    public PlayerExploder getPlayerExploder(){
+    public PlayerExploder getPlayerExploder() {
         return this.pe;
     }
 
-    public void setActivated(boolean flag){
-        this.activated = flag;
+    public RealConfiguration getRealConfig() {
+        return this.config;
     }
-    public boolean getActivated(){
-        return this.activated;
-    }
-
-    public void setPower(int power){
-        this.power = power;
-    }
-    public int getPower(){
-        return this.power;
-    }
-
-    public void setDamage(double damage) {
-        this.damage = damage;
-    }
-    public double getDamage() {
-        return damage;
-    }
-
-    public void setProbability(double probability){
-        this.probability = probability;
-    }
-    public double getProbability() {
-        return probability;
-    }
-
-    public void setPos1(int x, int z){
-        this.x1 = x;
-        this.z1 = z;
-    }
-    public int getX1(){
-        return this.x1;
-    }
-    public int getZ1(){
-        return this.z1;
-    }
-
-    public void setPos2(int x, int z){
-        this.x2 = x;
-        this.z2 = z;
-    }
-    public int getX2(){
-        return this.x2;
-    }
-    public int getZ2(){
-        return this.z2;
-    }
-
-
-    // アイテムクリックで爆弾解除
-    // アイテムクリックで爆弾設置
-
-    // 設定記録
 }
