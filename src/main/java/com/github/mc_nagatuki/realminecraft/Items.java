e400346fa10dda3c99c9f30522c00b8fd2a6a768
McNagatuki <kikihiji.fuumi5447491@gmail.com>
2020-11-26 01:44:03 +0900

    review: 共通する部分をくくりだす

--- src/main/java/com/github/mc_nagatuki/realminecraft/Items.java
+++ src/main/java/com/github/mc_nagatuki/realminecraft/Items.java
@@ -3,24 +3,21 @@ package com.github.mc_nagatuki.realminecraft;
 import org.bukkit.Material;
 
 public class Items {
-    public static class Detector{
-        public static final Material material = Material.BLAZE_ROD;
-        public static final String id = "minecraft:blaze_rod";
-        public static final String name = "地雷探知機";
-        public static final String color = "green";
+    public static class ItemType {
+        public final Material material;
+        public final String id;
+        public final String name;
+        public final String colo;
+
+        public ItemType(Material material, String id, String name, String colo) {
+            this.material = material;
+            this.id = id;
+            this.name = name;
+            this.colo = colo;
+        }
     }
 
-    public static class Installer{
-        public static final Material material = Material.BOWL;
-        public static final String id = "minecraft:bowl";
-        public static final String name = "ほんものの地雷";
-        public static final String color = "red";
-    }
-
-    public static class Sweeper{
-        public static final Material material = Material.WHEAT;
-        public static final String id = "minecraft:wheat";
-        public static final String name = "マインスイーパー";
-        public static final String color = "yellow";
-    }
+    public static final ItemType Detector = new ItemType(Material.BLAZE_ROD, "minecraft:blaze_rod", "地雷探知機", "green");
+    public static final ItemType Installer = new ItemType(Material.BOWL, "minecraft:bowl", "ほんものの地雷", "red");
+    public static final ItemType Sweeper = new ItemType(Material.WHEAT, "minecraft:wheat", "マインスイーパー", "yellow");
 }
